333519429cf286afba7e8a70ca4d88f0b9db8ec9
sebgrebe <sebbecker@gmx.net>
2018-04-07 23:26:16 +0300

    fix twitter cb for prod

--- auth/twitterRequestToken.js
+++ auth/twitterRequestToken.js
@@ -3,10 +3,11 @@ const request = require('request')
 const getParams = require('../modules/getParams.js')
 
 module.exports = (cb) => {
+    const root = (process.env.NODE_ENV === "production") ? 'https://charityles.herokuapp.com/' : 'http://localhost:8081/'
     request.post({
       url: 'https://api.twitter.com/oauth/request_token',
       oauth: {
-        oauth_callback: encodeURI('http://localhost:8081/sign-in-with-twitter'),
+        oauth_callback: encodeURI(root + 'sign-in-with-twitter'),
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET
       }
